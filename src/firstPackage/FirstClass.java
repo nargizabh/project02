f28256754be662dd6f0a1969f280631206927d41
Nargiza Berdyyeva <nargiza.berdyeva@gmail.com>
2020-11-08 18:40:13 +0100

    fifth commit

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -8,8 +8,13 @@ public class FirstClass {
 		System.out.println("third commit from local master");
 		System.out.println("first commit from remote master branch");
 		System.out.println("second commit from remote master branch");
-		System.out.println("Third commit from remote master branch");
+		System.out.println("third commit from remote master branch");
+		
+		System.out.println("Here is a merge conflict I create from local master");
+		
 		System.out.println("Here is the conflict we have from remote master branch");
+		System.out.println("Here is another conflict from remote master branch");
+		
 		
 
 	}
